edf87f4dfccbc2815cf485c2f0a18d535f47a06a
Ilona <ilona.fursova@apriorit.com>
2024-12-19 14:17:35 +0200

    Update main.tf

--- hw10/main.tf
+++ hw10/main.tf
@@ -10,7 +10,6 @@ resource "azurerm_resource_group" "example_rg" {
   location = "Canada Central"
 }
 
-
 # Virtual Network
 resource "azurerm_virtual_network" "example_vnet" {
   name                = "example-vnet"
@@ -19,29 +18,35 @@ resource "azurerm_virtual_network" "example_vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-# 2 Subnets
+# Subnet
 resource "azurerm_subnet" "example_subnet" {
-  count               = 2
-  name                = "example-subnet-${count.index}"
+  name                = "example-subnet"
   resource_group_name = azurerm_resource_group.example_rg.name
   virtual_network_name = azurerm_virtual_network.example_vnet.name
-  address_prefixes    = ["10.0.${count.index}.0/24"]
+  address_prefixes    = ["10.0.0.0/24"]
 }
 
-# 2 network interfaces
+# Network Interface
 resource "azurerm_network_interface" "example_nic" {
-  count               = 2
-  name                = "example-nic-${count.index}"
+  name                = "example-nic"
   location            = azurerm_resource_group.example_rg.location
   resource_group_name = azurerm_resource_group.example_rg.name
 
   ip_configuration {
-    name                          = "example-ipconfig-${count.index}"
-    subnet_id                     = azurerm_subnet.example_subnet[count.index].id
+    name                          = "example-ipconfig"
+    subnet_id                     = azurerm_subnet.example_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
+# Public IP
+resource "azurerm_public_ip" "example_public_ip" {
+  name                         = "example-public-ip"
+  location                     = azurerm_resource_group.example_rg.location
+  resource_group_name          = azurerm_resource_group.example_rg.name
+  allocation_method            = "Dynamic"
+  domain_name_label            = "examplepublicip"
+}
 
 # Network Security Group
 resource "azurerm_network_security_group" "nsg" {
@@ -49,7 +54,7 @@ resource "azurerm_network_security_group" "nsg" {
   location            = azurerm_resource_group.example_rg.location
   resource_group_name = azurerm_resource_group.example_rg.name
 
-#HTTP Port Opened 
+  # HTTP Port Opened 
   security_rule {
     name                       = "WEB"
     priority                   = 1002
@@ -62,7 +67,7 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
-#SSH 
+  # SSH Port
   security_rule {
     name                       = "SSH"
     priority                   = 1001
@@ -74,39 +79,16 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-#Restricted to Apriorit IPs
-  security_rule {
-    name                       = "RestrictToAprioritIPs"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefixes    = ["85.198.144.37"] #Prituln server address
-    destination_address_prefix = "*"
-  }
-
 }
 
-# Subnet Network Security Group Association
-resource "azurerm_subnet_network_security_group_association" "example" {
-  count                     = length(azurerm_subnet.example_subnet)
-  subnet_id                 = azurerm_subnet.example_subnet[count.index].id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# Virtual Machines 1,2
-
+# VM
 resource "azurerm_linux_virtual_machine" "vm" {
-  count                 = 2
-  name                  = "example-vm-${count.index}"
-  resource_group_name = azurerm_resource_group.example_rg.name
-  location            = azurerm_resource_group.example_rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [azurerm_network_interface.example_nic[count.index].id]
+  name                  = "example-vm"
+  resource_group_name   = azurerm_resource_group.example_rg.name
+  location              = azurerm_resource_group.example_rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.example_nic.id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -119,9 +101,18 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
-  #Path to public key
+
+  # Path to public key
   admin_ssh_key {
     username   = "azureuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDrG7Pmk7ddQjCbcEOKJWNWKBdMhOcrp7T1oUohqrcyjOyWsLtK7+Cs5mpZtLlI7YmZXCZ0v6HbYcSO8hxfF2EITqr6/laEbfCfIPW+RUxG19szzUgoEtiupITyzxGSXBtIRbSYHpKJT1di3tCcOOgL+ecoafUbsySGXuHXHhpHU29mNHiE6GVtC/tRNfQhQ83rfIR6v+lYs+R4HiYpk89Fff7WtWUubOj8tmEhadho3fBw7+yBkswqeYV9fkXqKsFp5kX57/qI4ytt29r2M8VFz7g+PjWSOeSWqf1H1E5QsxDSyPYHlCHyIywq1adlEVrWXZUEH+eY6CTBGygmPVQD"  
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDrG7Pmk7ddQjCbcEOKJWNWKBdMhOcrp7T1oUohqrcyjOyWsLtK7+Cs5mpZtLlI7YmZXCZ0v6HbYcSO8hxfF2EITqr6/laEbfCfIPW+RUxG19szzUgoEtiupITyzxGSXBtIRbSYHpKJT1di3tCcOOgL+ecoafUbsySGXuHXHhpHU29mNHiE6GVtC/tRNfQhQ83rfIR6v+lYs+R4HiYpk89Fff7WtWUubOj8tmEhadho3fBw7+yBkswqeYV9fkXqKsFp5kX57/qI4ytt29r2M8VFz7g+PjWSOeSWqf1H1E5QsxDSyPYHlCHyIywq1adlEVrWXZUEH+eY6CTBGygmPVQD"
   }
+
+  # Public IP association
+  depends_on = [azurerm_public_ip.example_public_ip]
+}
+
+# Output public IP
+output "vm_public_ip" {
+  value = azurerm_public_ip.example_public_ip.ip_address
 }
